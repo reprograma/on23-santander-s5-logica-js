091b9dedab52c5188beeb55aa6c5bc496ac093f7
Ana Beatriz Morita <anabemorita@gmail.com>
2023-04-22 17:11:48 -0300

    Adicionar exercicio de aula

---
+++ aula/ex1.js
@@ -0,0 +1,11 @@
+let valorReais = Number(prompt('Qual o valor em reais a ser convertido para dolar?'))
+
+let taxaDeCambio = 5.30 // opcional
+
+let resultado = valorReais / taxaDeCambio
+// let resultado = valorReais / 5.30
+
+alert('valor convertido em dolar é ' + resultado)
+console.log('valor convertido em dolar é ' , resultado)
+
+// alert('valor convertido em dolar é ' + valorReais / taxaDeCambio)
