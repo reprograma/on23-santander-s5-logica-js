6f563be4141f471e623ff606ad1c265e232a1fce
Mia Azevedo <mialuzazevedo@gmail.com>
2023-04-22 20:26:29 -0400

    atualização. adcionada classificação de notas

--- exercicios/para-casa/exercicio-medias/js/script.js
+++ exercicios/para-casa/exercicio-medias/js/script.js
@@ -4,16 +4,18 @@ function calcularMedia() {
     let boxNota2 = document.getElementById("box-num2");
     let res = document.getElementById('res')
 
+    let nota1 = Number(boxNota1.value)
+    let nota2 = Number(boxNota2.value)
+    let media = (nota1 + nota2) / 2;
+
     if (boxNota1.value.length == 0 || boxNota2.value.length == 0) {
         alert('ERRO! COMPLETE TODOS OS CAMPOS.')
+    } else if (media >= 7) {
+        res.innerHTML = `Resultado: ${media}. Você foi aprovado(a). \u{1F601}`
+    } else if (media > 5 && media < 7) {
+        res.innerHTML = `Resultado: ${media}. Você está de recuperação. \u{1F625}`  
     } else{
-        let nota1 = Number(boxNota1.value)
-        let nota2 = Number(boxNota2.value)
-    
-        let media = (nota1 + nota2) / 2;
-    
-        res.innerHTML = `Resultado: ${media}` 
+        res.innerHTML = `Resultado: ${media}. Você foi reprovado(a) \u{1F641}`
     }
-
    
 }
