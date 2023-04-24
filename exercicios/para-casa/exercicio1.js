3ce2a2cd94aee265a8f2054797cccebeb9a4859e
Aline Beltrao <alinebelltrao@gmail.com>
2023-04-24 00:35:40 -0300

    primeiro commit

---
+++ exercicios/para-casa/exercicio1.js
@@ -0,0 +1,15 @@
+var nota1 = parseInt(prompt("Coloque a primeira nota:"))
+var nota2 = parseInt(prompt("Coloque a segunda nota:"))
+var media = (nota1 + nota2)/2
+
+if (media >=7) {
+    alert("você foi aprovado com a média " + media)
+} else if (media >= 5) {
+    alert("você está em recuperação com a média " + media)
+} else{
+    alert("você foi reprovado, média " + media)
+}
+    
+
+
+
