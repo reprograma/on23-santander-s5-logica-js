09a17703445542a38420fd20c82d4456b8f1a7f5
Manoelly <manoellymaria@gmail.com>
2023-04-27 16:50:36 -0300

    Adição dos exercícios resolvidos: classe e casa.

---
+++ exercicios/para-sala/ex1.js
@@ -0,0 +1,5 @@
+let valorReais = parseFloat(prompt('Qual é o valor que você quer converter ?'));
+
+let valorConvertido = valorReais / 5.3;
+
+alert('O valor convertido em dólares é de: ' + valorConvertido);
