bc31ed22a0e8968dee7e48fb08bca2109fa8aeb5
Flaviana <flaviferraz@yahoo.com.br>
2023-04-27 12:14:42 -0300

    resolução exercicio semana 5 js

---
+++ exercicios/para-casa/script.js
@@ -0,0 +1,19 @@
+let nota1 =+ prompt("Insira a nota 1");
+
+let nota2 =+ prompt("Insira a nota 2");
+
+let media = (nota1 + nota2)/2
+
+if (media >= 7) {
+    alert("Aluno Aprovado")
+} else if (media >= 5) {
+    alert("Aluno em Recuperação.")
+} else {
+    alert("Aluno Reprovado.")
+}
+
+
+
+
+
+
