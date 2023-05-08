99a3a1c11bf374c633d5193f6b4f5bfa56275d15
Aline Beltrao <alinebelltrao@gmail.com>
2023-05-08 18:43:35 -0300

    excluindo comentarios

--- exercicios/para-casa/exercicio1.js
+++ exercicios/para-casa/exercicio1.js
@@ -1,4 +1,4 @@
-/*var nota1 = parseInt(prompt("Coloque a primeira nota:"))
+var nota1 = parseInt(prompt("Coloque a primeira nota:"))
 var nota2 = parseInt(prompt("Coloque a segunda nota:"))
 var media = (nota1 + nota2)/2
 
@@ -8,13 +8,8 @@ if (media >=7) {
     alert("você está em recuperação com a média " + media)
 } else{
     alert("você foi reprovado, média " + media)
-}*/
+}
 
 
     
 
-let emailCadastrado = prompt("insira seu email")
-let senhaCadastrada = prompt("insira sua senha")
-
-emailCadastrado
-senhaCadastrada
